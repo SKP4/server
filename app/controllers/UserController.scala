7486ece7764d2e439ff28816a8c7085857e45d5c
1ambda <1amb4a@gmail.com>
2015-06-16 23:52:19 +0900

    add implicit userWrites

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,16 +1,30 @@
 package controllers
 
-import play.api.libs.json.{Json, JsValue}
+import play.api.libs.json.{Writes, Json, JsValue}
 import play.api.mvc.{Action, Controller}
 
+case class User(id: String, name: String, age: Int)
+
+object User {
+  implicit val userWrites = new Writes[User] {
+    override def writes(user: User) = Json.obj(
+      "id" -> user.id,
+      "name" -> user.name,
+      "age" -> user.age
+    )
+  }
+}
+
 class UserController extends Controller {
+  import User._
+
   def getUser = Action {
     val user: JsValue = Json.parse("""
-         {  "id": "1ambda",
+         {  "id": "1ambda!",
             "name": "Hoon",
             "age": 27
          }""")
 
-    Ok(user)
+    Ok(Json.toJson(User("1ambda", "Hoon", 27)))
   }
 }
