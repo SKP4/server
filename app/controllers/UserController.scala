30329f52d07ce72952248af93e1d827288dfbee1
1ambda <1amb4a@gmail.com>
2015-06-17 23:45:41 +0900

    add reads[UserSignupRequest]

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,30 +1,41 @@
 package controllers
 
+
+import java.util.concurrent.atomic.{AtomicLong, AtomicReference}
+
+import akka.actor.FSM.->
+import domain.{UserSignupRequest, User}
 import play.api.libs.json.{Writes, Json, JsValue}
 import play.api.mvc.{Action, Controller}
 
-case class User(id: String, name: String, age: Int)
-
-object User {
-  implicit val userWrites = new Writes[User] {
-    override def writes(user: User) = Json.obj(
-      "id" -> user.id,
-      "name" -> user.name,
-      "age" -> user.age
-    )
-  }
-}
+import User._
 
 class UserController extends Controller {
-  import User._
 
-  def getUser = Action {
-    val user: JsValue = Json.parse("""
-         {  "id": "1ambda!",
-            "name": "Hoon",
-            "age": 27
-         }""")
+  var users = Map.empty[String, User]
+  users += "Hoon" -> User(1L, "Hoon", 27)
+  users += "Noh" -> User(2L, "Noh", 20)
 
-    Ok(Json.toJson(User("1ambda", "Hoon", 27)))
+  var uid = new AtomicLong(0L)
+
+  def createUid: Long = uid.incrementAndGet()
+
+  def getUser(uid: Long) = Action {
+    if (users.values.f)
+      Ok(Json.toJson(users(uid)))
+    else NotFound
+  }
+
+  def getAllUsers = Action {
+    Ok(Json.toJson(users.values))
+  }
+
+  def createUser = Action(parse.json) { request =>
+    val signupReq@UserSignupRequest(name, age) = request.body.as[UserSignupRequest]
+    val uid = createUid
+
+    val created = User(uid, name, age)
+    users += name -> created
+    Ok(Json.toJson(created))
   }
 }
---
+++ app/domain/User.scala
@@ -0,0 +1,20 @@
+package domain
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+case class User(uid: Long, name: String, age: Int)
+case class UserSignupRequest(name: String, age: Int)
+
+object User {
+  implicit val userWrites = (
+    (JsPath \ "uid").write[Long] and
+    (JsPath \ "name").write[String] and
+      (JsPath \ "age").write[Int]
+    )(unlift(User.unapply))
+
+  implicit val userSignupRequestReads = (
+    (JsPath \ "name").read[String] and
+    (JsPath \ "age").read[Int]
+    )(UserSignupRequest.apply _)
+}
