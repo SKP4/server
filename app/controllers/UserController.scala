bffdc1995abaab1fd95fcc6e4abe9d1a0b590d0a
1ambda <1amb4a@gmail.com>
2015-06-18 02:08:21 +0900

    replace users signature to Map[Long, User]

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -10,15 +10,24 @@ import play.api.mvc.{Action, Controller}
 
 import User._
 
+import scala.collection.immutable.Iterable
+
 class UserController extends Controller {
 
-  var users = Map.empty[String, User]
-  users += "Hoon" -> User(1L, "Hoon", 27)
-  users += "Noh" -> User(2L, "Noh", 20)
+  var users = Map.empty[Long, User]
+  users += 1L -> User(1L, "Hoon", 27)
+  users += 2L -> User(2L, "Noh", 20)
 
-  var uid = new AtomicLong(0L)
+  var uid = new AtomicLong(2L)
 
   def createUid: Long = uid.incrementAndGet()
+
+  def getUser(uid: Long) = Action {
+    users.get(uid) match {
+      case Some(user) => Ok(Json.toJson(user))
+      case None       => NotFound
+    }
+  }
 
   def getAllUsers = Action {
     Ok(Json.toJson(users.values))
@@ -27,12 +36,13 @@ class UserController extends Controller {
   def createUser = Action(parse.json) { request =>
     val signupReq@UserSignupRequest(name, age) = request.body.as[UserSignupRequest]
     val uid = createUid
-    val created = User(uid, name, age)
 
-    if (users.contains(name)) Conflict
-    else {
-      users += name -> created
-      Ok(Json.toJson(created))
+    users.values.filter(_.name == name).toList match {
+      case List(user) => Conflict
+      case Nil        =>
+        val created = User(uid, name, age)
+        users += uid -> created
+        Ok(Json.toJson(created))
     }
   }
 }
