eb7ecd0fe81986549eedb57c61ff13923f369b94
1ambda <1amb4a@gmail.com>
2015-06-20 14:16:23 +0900

    add .travis.yml, remove integration test

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -11,13 +11,10 @@ import play.api.test.Helpers._
  */
 class IntegrationSpec extends Specification {
 
-  "Application" should {
-
-    "work from within a browser" in new WithBrowser {
-
-      browser.goTo("http://localhost:" + port)
-
-      browser.pageSource must contain("Your new application is ready.")
-    }
-  }
+//  "Application" should {
+//    "work from within a browser" in new WithBrowser {
+//      browser.goTo("http://localhost:" + port)
+//      browser.pageSource must contain("Your new application is ready.")
+//    }
+//  }
 }
