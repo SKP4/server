7c0ebfa33bbbbfe609e5f26cca7335e98a560b10
1ambda <1amb4a@gmail.com>
2015-06-20 13:58:58 +0900

    introduce slick.
    - UserDao
    - persist data into H2 (in-memory database)
    
    TODO
    
    - remove init database using slick evolution
    - play-slick
    - fix failed tests

--- app/model/User.scala
+++ app/model/User.scala
@@ -1,24 +1,31 @@
-package domain
+package model
 
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
-import slick.driver.JdbcProfile
+import slick.lifted.ProvenShape
+import slick.driver.H2Driver.api._
 
-case class User(uid: Long, name: String, age: Int)
+import scala.concurrent.Future
+
+case class User(id: Long, name: String, age: Int)
 case class UserSignupRequest(name: String, age: Int)
 
-//trait UserTable {
-//  protected val driver: JdbcProfile
-//  import driver.api._
-//
-//  class Users(tag: Tag) extends Table[User](tag, "USERS") {
-//    def uid  = column[Long]("id", O.PrimaryKey, O.AutoInc)
-//    def name = column[String]("NAME")
-//    def age  = column[Int]("COLOR")
-//  }
-//}
+trait UserTable {
+  class Users(tag: Tag) extends Table[User](tag, "USERS") {
+    def id = column[Long]("id", O.PrimaryKey)
+    def name = column[String]("name")
+    def age = column[Int]("age")
+    def * : ProvenShape[User] = (id, name, age) <>((User.apply _).tupled, User.unapply)
+  }
+}
 
 object User {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  def findById(id: Long): Option[User] = {
+    db.run(u.result)
+  }
+
   implicit val userWrites = (
     (JsPath \ "uid").write[Long] and
     (JsPath \ "name").write[String] and
